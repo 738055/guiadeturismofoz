e647c86138d7ebc5019449e145314bc516d096fc
Thomas William <thoomaswill@gmail.com>
2025-12-13 09:57:06 -0300

    okok

--- app/admin/combos/edit/[id]/page.tsx
+++ app/admin/combos/edit/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { AdminComboForm } from '../../../ComboForm';
+import { AdminComboForm } from '../../ComboForm';
 
 export default function EditComboPage({ params }: { params: { id: string } }) {
   return <AdminComboForm comboId={params.id} />;
